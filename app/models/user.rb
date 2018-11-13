ab342c43d86026b05aecd70a9a05fe871018a0c6
dniman <dade.niman@gmail.com>
2018-11-13 23:44:34 +0300

    change has_and_belongs_to_many to has_many and belongs_to

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :answers, join_table: :user_answers
+  has_many :answers
 
   def user_tests(level)
     Test.distinct.joins(questions: [:answers]).where("tests.level = ?", level)
