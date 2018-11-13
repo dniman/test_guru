73932df546e7b905ebc11ffee8c1f565d6b4b2bb
dniman <dade.niman@gmail.com>
2018-11-13 22:25:56 +0300

    user answers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :tests, :join_table => :user_tests
+  has_and_belongs_to_many :answers, join_table: :user_answers
 end
+
--- db/migrate/20181113175609_create_user_answers.rb
+++ db/migrate/20181113175609_create_user_answers.rb
@@ -1,8 +1,7 @@
-class CreateUserTests < ActiveRecord::Migration[5.2]
+class CreateUserAnswers < ActiveRecord::Migration[5.2]
   def change
-    create_table :user_tests, id: false do |t|
+    create_table :user_answers do |t|
       t.references :user, index: true, foreign_key: { to_table: :users }, null: false
-      t.references :test, index: true, foreign_key: { to_table: :tests }, null: false
       t.references :answer, index: true, foreign_key: { to_table: :answers }, null: false
     end
   end
