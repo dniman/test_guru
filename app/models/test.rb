25b9619115df1b1181442cf663d6e73d8515438f
dniman <dade.niman@gmail.com>
2018-11-13 23:17:04 +0300

    associations with two methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+
+  def self.in_descending_order(category_title)
+    self.joins(:category).where("categories.title = ?", category_title).order("tests.title desc").pluck("tests.title")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :answers, join_table: :user_answers
+
+  def user_tests(level)
+    Test.distinct.joins(questions: [:answers]).where("tests.level = ?", level)
+  end
 end
 
