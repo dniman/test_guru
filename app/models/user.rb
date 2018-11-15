9e737dc4fe36f677c466ef8b4d1e75a680a0cb87
dniman <dade.niman@gmail.com>
2018-11-15 18:15:22 +0300

    Change user_tests to tests_by

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests, :join_table => :user_tests
 
-  def user_tests(level)
+  def tests_by(level)
     tests.where("tests.level = ?", level)
   end
 end
