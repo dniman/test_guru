028a093cf981d2914159407a377cce8dc6400b76
dniman <dade.niman@gmail.com>
2018-11-17 13:42:26 +0300

    associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'   
 
   def self.in_descending_order(category_title)
     joins(:category).where(categories: { title: category_title }).order("tests.title desc").pluck("tests.title")
