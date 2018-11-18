422d0d81b4e1a7b414cb2c7e4994161b4472cc77
dniman <dade.niman@gmail.com>
2018-11-18 10:30:09 +0300

    refer to my_tests, where the user is an author of this tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests, :join_table => :user_tests
-  has_many :tests, class_name: 'Test', foreign_key: 'author_id'   
+  has_many :my_tests, class_name: 'Test', foreign_key: 'author_id'   
 
   def tests_by(level)
     tests.where(level: level)
