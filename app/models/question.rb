bc4917a3912a4ea177ec7ebdcb2912832954b170
dniman <dade.niman@gmail.com>
2018-11-19 23:03:43 +0300

    validate answers count

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,11 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :body, presence: true
+  validate :validate_answers
+     
+  private 
+  def validate_answers
+    errors.add(:answers, "too little") if answers.size < 1
+    errors.add(:answers, "too much") if answers.size > 4
+  end
 end
