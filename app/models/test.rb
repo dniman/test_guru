c669dfdf2933e2ff88cb87dba41e21910e9e6169
dniman <dade.niman@gmail.com>
2018-11-19 20:52:49 +0300

    select tests by difficulty

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,14 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'   
   has_many :questions
+               
+  LOW_DIFFICULTY = 0..1
+  MIDDLE_DIFFICULTY = 2..4
+  HIGH_DIFFICULTY = 5..Float::INFINITY
+
+  scope :low_levels, -> { where(level: LOW_DIFFICULTY) }
+  scope :middle_levels, -> { where(level: MIDDLE_DIFFICULTY) }
+  scope :high_levels, -> { where(level: HIGH_DIFFICULTY) }
 
   def self.in_descending_order(category_title)
     joins(:category).where(categories: { title: category_title }).order("tests.title desc").pluck("tests.title")
