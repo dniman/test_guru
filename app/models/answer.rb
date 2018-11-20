710c8138facb95c893732acbef718cbd69a357c0
dniman <dade.niman@gmail.com>
2018-11-20 20:16:46 +0300

    move validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,18 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :correct_records, -> { where(correct: true) } 
+  scope :question_answers, ->(question) { where(question: question) }
 
   validates :body, presence: true
+  validate :validate_answer_count
+     
+  private 
+  def validate_answer_count
+    errors.add(:answers, "too little") if answer_count < 1
+    errors.add(:answers, "too much") if answer_count > 4
+  end
+
+  def answer_count
+    self.class.question_answers(question).size
+  end
 end
