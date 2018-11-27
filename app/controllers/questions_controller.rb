19a60fc4ce5447b3028f7da2b13867c09a6b3cdb
dniman <dade.niman@gmail.com>
2018-11-27 22:34:19 +0300

    QuestionsControllerapp/controllers/tests_controller.rbshow

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,24 @@
 class QuestionsController < ApplicationController
   def index
-    @test = Test.find(params[:test_id]) 
     msg = ["#{@test.title}:"]
-    @test.questions.each do |q| 
+    test.questions.each do |q| 
       msg << "  #{q.body}" 
     end
 
     render plain: msg.join("\n")  
+  end
+
+  def show 
+    question = Question.find(params[:id])
+    msg = [] 
+    msg << "id - #{question.id}"
+    msg << "body - #{question.body}" 
+         
+    render plain: msg.join("\n") 
+  end
+
+  private
+  def test
+    Test.find(params[:test_id])
   end
 end
