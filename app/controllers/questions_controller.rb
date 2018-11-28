fce93153299ffa301db19b12d2b922efe3a254b3
dniman <dade.niman@gmail.com>
2018-11-28 14:09:21 +0300

    destroy, before_action, rescue_from and refactoring

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,16 @@
 class QuestionsController < ApplicationController
-  def index
-    test = Test.find(params[:test_id])
 
-    render plain: test.questions.inspect
+  before_action :find_test, only: :index
+  before_action :find_question, only: %i[show destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render plain: @test.questions.inspect
   end
 
   def show 
-    question = Question.find(params[:id])
-         
-    render plain: question.inspect 
+    render plain: @question.inspect 
   end
 
   def new 
@@ -19,15 +21,32 @@ class QuestionsController < ApplicationController
 
     render plain: question.inspect 
   end
-
+          
+  def destroy
+    render plain: "Question was deleted" 
+  end
+       
   private
+  
+  def rescue_with_question_not_found
+    render plain: 'Question was not found' 
+  end
 
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+  
   def question_params
     parameters.permit(:test_id, :body) 
   end
 
   def parameters
     raw_parameters = { test_id: params[:test_id], body: params[:question][:body] }
+
     ActionController::Parameters.new(raw_parameters)
   end
 end
