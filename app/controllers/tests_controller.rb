a0bb2a38a47a9baef699de4dee888affc9b21418
dniman <dade.niman@gmail.com>
2018-12-02 22:55:58 +0300

    rails helpers

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -38,6 +38,13 @@ class TestsController < ApplicationController
     end
   end
 
+  def destroy
+    @test = Test.find(params[:id])
+
+    @test.destroy
+    redirect_to tests_path
+  end
+
   private
 
   def test_params
