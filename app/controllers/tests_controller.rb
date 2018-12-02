f955c7f489c932f4445d52c6f8510155923b7204
dniman <dade.niman@gmail.com>
2018-12-02 20:46:50 +0300

    test controller actions and views

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,50 @@
+class TestsController < ApplicationController
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def create
+    @test = Test.new(test_params)
+    @test.author_id = current_user.id
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:category_id, :title, :level)
+  end
+
+  def current_user
+    @user = User.find_by(name: "Some Admin")
+  end
+end
