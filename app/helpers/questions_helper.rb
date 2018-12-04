e3a577ffa5e2ff193366dc6e21ef33bb80040118
dniman <dade.niman@gmail.com>
2018-12-05 01:59:53 +0300

    tasks

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_header(test, question)
+    if question.nil?
+      "Create New #{test.title} Question"
+    else
+      "Edit #{test.title} Question"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: [:index]
+    put :question, path: 'questions/:question_id'
   end
 end
 
