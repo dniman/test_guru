929f8edf6b67bde7866299250ea119eaa4f34ddf
dniman <dade.niman@gmail.com>
2018-12-14 13:14:41 +0300

    refactoring

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -4,4 +4,9 @@ module TestPassagesHelper
   def passed_questions(test_passage)
     test_passage.correct_questions/test_passage.test.questions.count * 100
   end
+
+  def test_passed?(test_passage)
+    return true if passed_questions(test_passage) >= TEST_PASSED 
+    false
+  end
 end
