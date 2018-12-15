4d42ebb23118908a10a890bda85d400437fab18d
dniman <dade.niman@gmail.com>
2018-12-15 19:23:17 +0300

    refactoring with notes

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,12 +1,2 @@
 module TestPassagesHelper
-  TEST_PASSED = 85
-
-  def passed_questions(test_passage)
-    test_passage.correct_questions/test_passage.test.questions.count * 100
-  end
-
-  def test_passed?(test_passage)
-    return true if passed_questions(test_passage) >= TEST_PASSED 
-    false
-  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,9 +3,10 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
           
-  before_validation :before_validation_set_first_question, on: :create
-  before_save :before_save_set_next_question, on: :save
-  
+  before_save :before_save_set_next_question, on: :save 
+
+  TEST_PASSED = 85
+
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
@@ -22,14 +23,18 @@ class TestPassage < ApplicationRecord
     test.questions.find_index(current_question) + 1
   end
 
-  private
-
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present? 
+  def success?
+    progress >= TEST_PASSED 
   end
 
+  def progress
+    correct_questions/test.questions.count * 100.0
+  end
+
+  private
+
   def before_save_set_next_question
-    self.current_question = next_question if persisted?
+    self.current_question = persisted? ? next_question : test.questions.first
   end
 
   def correct_answer?(answer_ids)
