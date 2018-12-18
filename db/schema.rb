a424f68ce2d8eab3425e8c7e028f6806944817e5
dniman <dade.niman@gmail.com>
2018-12-18 16:22:18 +0300

    password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_15_171204) do
+ActiveRecord::Schema.define(version: 2018_12_18_130202) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_12_15_171204) do
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
