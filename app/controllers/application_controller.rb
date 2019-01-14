6d4fe64546004c469924e09f928214646cc689e3
dniman <dade.niman@gmail.com>
2019-01-14 20:32:37 +0300

    redirect after sign_in and some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def after_sign_in_path_for(resource)
+    return admin_tests_path if current_user.is_a?(Admin)
+    admin_tests_path
+  end
 end
