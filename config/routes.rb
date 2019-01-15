3c622158075e1f9efdee6397e9022d95b46b90a1
dniman <dade.niman@gmail.com>
2019-01-15 14:35:26 +0300

    namespace admin for answers and questions

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
   resources :tests, only: :index do
-    resources :questions, shallow: true, except: [:index] do
-      resources :answers, shallow: true, except: [:index]
+    namespace :admin do
+      resources :questions, shallow: true, except: [:index] do
+        resources :answers, shallow: true, except: [:index]
+      end
     end
 
     member do
