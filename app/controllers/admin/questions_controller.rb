cc8d2125531e697538fab5968cf3b43a46d6c6bf
dniman <dade.niman@gmail.com>
2019-01-16 20:52:12 +0300

    remove redundant callback

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,6 +1,5 @@
 class Admin::QuestionsController < Admin::BaseController
 
-  before_action :authenticate_user!
   before_action :find_test, only: %i[new create]
   before_action :find_question, only: %i[show edit update destroy]
 
