6dea30b35873c55416a718f806ef7f0727d35018
dniman <dade.niman@gmail.com>
2019-01-17 11:53:12 +0300

    add first_name in sign_in flash message

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }, controllers: { sessions: "sessions" }
 
   resources :tests, only: :index do
     namespace :admin do
