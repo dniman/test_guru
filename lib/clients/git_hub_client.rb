7168e07d20a7ca2bc2a26500b812afcf902fe4a8
dniman <dade.niman@gmail.com>
2019-02-26 14:28:18 +0300

    .env for resolving security issues

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,7 +1,6 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '642a60c06233a0b542863c0f8776899233d56b1c'
      
   def initialize
     @http_client = setup_http_client
@@ -9,7 +8,7 @@ class GitHubClient
   
   def create_gist(params)
     @http_client.post('gists', params) do |request|
-      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+      request.headers['Authorization'] = "token #{ENV['ACCESS_TOKEN']}"
       request.headers['Content-Type'] = 'application/json'
       request.body = params.to_json
     end
