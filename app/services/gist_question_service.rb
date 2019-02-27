28e3f1cbb8580cfdce6830dc2791f2b7f387dbda
dniman <dade.niman@gmail.com>
2019-02-27 15:51:26 +0300

    I18n

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -14,7 +14,7 @@ class GistQuestionService
 
   def gist_params
     {
-      description: "A question about #{@test.title} from TestGuru",
+      description: I18n.t("services.description", title: @test.title),
       files: {
         'test-guru-question.txt': {
           content: gist_content
