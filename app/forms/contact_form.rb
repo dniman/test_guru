0c8b7560a9b078a81ffc3d08befc8c6d7f75505f
dniman <dade.niman@gmail.com>
2019-04-02 20:43:05 +0300

    ActiveModel::Model

--- app/forms/contact_form.rb
+++ app/forms/contact_form.rb
@@ -1,27 +1,10 @@
 class ContactForm
-  include ActiveModel::Conversion
-  include ActiveModel::AttributeMethods
-  include ActiveModel::Validations
-  extend ActiveModel::Naming
-  extend ActiveModel::Translation
+  include ActiveModel::Model
 
-  attr_reader :name, :email, :message_body
+  attr_accessor :name, :email, :message_body
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :message_body, presence: true
 
-  def initialize(attr = {})
-    @name = attr[:name]
-    @email = attr[:email]
-    @message_body = attr[:message_body]
-  end
-
-  def id
-    1
-  end
-
-  def permitted?
-    false
-  end
 end
