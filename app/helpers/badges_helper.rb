dc500c84befe69c3350b9ee691ca3b23608f8b81
dniman <dade.niman@gmail.com>
2019-05-06 16:22:04 +0300

    rename users_helper -> badges_helper

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,4 +1,4 @@
-module UsersHelper
+module BadgesHelper
   def badge_icon(badge_id)
     file_name = Badge.where(id: badge_id).pluck(:file_name).first
     image_tag "/#{file_name}", style: "height: 32px; width: 32px"
